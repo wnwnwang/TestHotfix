35362682cc5f96f6ab04b9553aa0cc72e4aea62f
wnwnwang <wnwnwang@sina.com>
2023-01-10 16:11:39 +0800

    feat: 文字修改

--- app/src/main/java/com/vyiot/testhotfix/MainActivity.java
+++ app/src/main/java/com/vyiot/testhotfix/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btnShow = findViewById(R.id.btn_show);
         btnShow.setOnClickListener(v -> {
-            Toast.makeText(MainActivity.this, "你好啊！！！！", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MainActivity.this, "hello, world!!!", Toast.LENGTH_SHORT).show();
         });
 
         Button md5 = findViewById(R.id.btn_md5);
